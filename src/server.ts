813b14c8169e0971f822608c1501ebb4dbd77db7
Aureane Moraes <aureane.moraes@outlook.com>
2023-03-30 13:02:20 -0300

    tratando variaveis de ambiente com a lib zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -9,6 +10,6 @@ app.get('/hello', async () => {
   return tables
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP SERVER RUNNING! :D')
 })
