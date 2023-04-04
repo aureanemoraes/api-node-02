a1e22b0de91481b3244ce625484009f3c0cdec4e
Aureane Moraes <aureane.moraes@outlook.com>
2023-04-04 12:55:07 -0300

    adicionada rota de summary e adicionada logica de cookie e sessionId

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,32 @@ import { z } from 'zod'
 import { randomUUID } from 'crypto'
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get('/summary', async (req, res) => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'ammount' })
+      .first()
+
+    return res.send({ summary })
+  })
+
+  app.get('/', async (req, res) => {
+    const transactions = await knex('transactions').select()
+
+    return res.send({ transactions })
+  })
+
+  app.get('/:id', async (req, res) => {
+    const transactionSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = transactionSchema.parse(req.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return res.send({ transaction })
+  })
+
   app.post('/', async (req, res) => {
     const transactionSchema = z.object({
       title: z.string(),
@@ -13,10 +39,18 @@ export const transactionsRoutes = async (app: FastifyInstance) => {
 
     const { title, amount, type } = transactionSchema.parse(req.body)
 
+    const sessionId = req.cookies.sessionId || randomUUID()
+
+    res.setCookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    })
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return res.status(201).send()
